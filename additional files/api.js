318d1bed80f52e2f95854698a00f44206df8a015
Sarthak Goel <63970210+sarty-definite@users.noreply.github.com>
2022-08-25 18:05:04 +0530

    Add files via upload

---
+++ additional files/api.js
@@ -0,0 +1,7 @@
+const api_content = {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "bhagavad-gita3.p.rapidapi.com",
+        "x-rapidapi-key": "ENTER YOUR API KEY HERE"
+    }
+}
